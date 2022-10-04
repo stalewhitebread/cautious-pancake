7046dfe8becc1080eda1663f953c0045687b0dc3
stalewhitebread <111912670+stalewhitebread@users.noreply.github.com>
2022-10-04 14:52:13 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -19,13 +19,11 @@ $(document).ready(function() {
 });
 
 const devProfile = {
-    "name": ["Barry", "Cumbie"],
-    "pic": "https://camo.githubusercontent.com/63dd79341795dcabe86885975fbdb6cd45203a9b8db05ee09ee54425a1435c99/68747470733a2f2f6672696e6b6961632e636f6d2f6d656d652f5330374530382f313030333736382e6a70673f6236346c696e65733d4945527649456b6761323576647942336147463043694169636d686c6447397961574e6862434a745a574675637a383d",
-    "title": "Gatlin Distinguished Professor of Innovation, Associate Professor, Computer Science & Information Systems, University of North Alabama",
-    "github": ["barrycumbie", "https://github.com/barrycumbie"],
-    "background": "Auburn Univeristy: MIS '99, Masters MIS '04, Doctorate MGT of IS & Innovation '08",
-    "interests": "JAM Stack, Frisbee",
-    "side-projects": "what? barely keeping up with my real ones.",
-    "must know dev tech": "Docker is super cool for containerization. Coggle is a new tool to try out.",
-    "skillz": "html5, css3, bs4, jQuery, js, json, so many wow...visual studio code, github, gist, markdown, codepen..."
+    "name": ["Ethan", "Criddle"],
+    "pic": "https://o.aolcdn.com/images/dar/5845cadfecd996e0372f/f135ee7745477b9242ad4f49b647bf1e9b7d83e5/aHR0cDovL28uYW9sY2RuLmNvbS9oc3Mvc3RvcmFnZS9taWRhcy9jMGZmZGVmYzFmNzlhZjdkNWIzMDU0ZDRmZjc0ZTc0OC8yMDM2MDAyMTIvZ01IVHRJWXJhYkE2ay5naWY=",
+    "title": "Certified Classic, Swim Club President",
+    "github": ["stalewhitebread", " https://github.com/stalewhitebread"],
+    "background": "High School Diploma, Interest in Computer Hardware",
+    "interests": "Organ Donor, Swimming",
+    "currently listening/watching": "Life of Pablo, Scissors Seven"
 };
